96a482d925771b9093ec2c420a767b6645b62824
Hugo Mendes <hugomoraismendes@gmail.com>
2021-10-31 17:21:25 -0300

    commit final

--- App.tsx
+++ App.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import { Home } from './src/screens/Home';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 
+import { AuthProvider } from './src/hooks/auth';
+import { Home } from './src/screens/Home';
+
 export default function App() {
-    const [fontsLoaded] = useFonts({
-        Roboto_400Regular,
-        Roboto_700Bold
-    });
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
 
-    if (!fontsLoaded) {
-        return <AppLoading />;
-    }
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
 
-    return (
-        <>
-            <StatusBar style="light" />
-            <Home />
-        </>
-    );
+  return (
+    <AuthProvider>
+      <StatusBar
+        style="light"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Home />
+    </AuthProvider>
+  );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import { useAuth } from '../../hooks/auth';
+import { UserPhoto } from '../UserPhoto';
+
 import { styles } from './styles';
+
 import LogoSvg from '../../assets/logo.svg';
 
 export function Header() {
-    return (
-        <View style={styles.container}>
-            <LogoSvg />
-            <Text>Sair</Text>
-        </View>
-    );
+  const { user, signOut } = useAuth();
+  return (
+    <View style={styles.container}>
+      <LogoSvg />
+
+      <View style={styles.logoutButton}>
+        {
+          user &&
+          <TouchableOpacity onPress={signOut}>
+            <Text style={styles.logoutText}>
+              Sair
+            </Text>
+          </TouchableOpacity>
+        }
+
+        <UserPhoto imageUri={user?.avatar_url} />
+      </View>
+    </View>
+  );
 }
